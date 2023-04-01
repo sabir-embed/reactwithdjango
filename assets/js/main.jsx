3e3ae713d8e3c0f2143ccadbfc530fbfc09cc881
Sabir Iqbal <sabir@micelio.local>
2023-04-01 10:46:32 +0530

    Left to fix footer

--- assets/js/main.jsx
+++ assets/js/main.jsx
@@ -1,1 +1,6 @@
-alert("vite template works!!")
+import React from 'react';
+import ReactDOM from "react-dom";
+import Chart from '../components/chart.jsx'
+
+const domContainer = document.querySelector('#chart-container');
+ReactDOM.render(<Chart/>, document.getElementById('chart-container'));
--- config/api.py
+++ config/api.py
@@ -1,7 +1,15 @@
 from ninja import NinjaAPI
+from random import randint
 
-api = NinjaAPI()
+api = NinjaAPI(csrf=True)
 
 @api.get("/test")
 def add(request):
     return {"result": "ok"}
+
+
+@api.get("/chartData",operation_id="getSensorData", tags=["charts"])
+def get_chart_data(request):
+    """Mock sensor data for frontend"""
+    chart_values = [randint(300, 1000) for i in range(7)]
+    return {"data": chart_values}
